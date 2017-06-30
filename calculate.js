6b66e0603c5e419ae15073ba674128841149b951
cleider <cleidergo@gmail.com>
2017-06-30 01:48:53 -0700

    When selecting a number display changes

--- calculate.js
+++ calculate.js
@@ -4,25 +4,37 @@
 
 
 $(document).ready(function(){
+
+    //alert("This is meant for mobile devices only. Thank you.");
     $(document).delegate(".ui-content", "touchmove", false);
 
-
-    $(document).on('touchstart','.col-sm-3, .col-sm-6', function(){
+    var index = 0;
+    $(document)
+        .on('touchstart','.col-sm-3, .col-sm-6', function(){
         $(this).fadeTo(0, 0.80);
-        console.log('mouse up')
 
     })
         .on('touchend','.col-sm-3, .col-sm-6',function(){
-            $(this).fadeTo(0, 1.20);
-            console.log('mouse down')
+            index = index +1;
+            console.log(index);
+            var this_elem = this;
+            $(this_elem).fadeTo(0, 1.20);
+            changeDisplay(this_elem, index)
+
 
         });
 
+    function changeDisplay(this_elem, index){
+        if (index === 1){
+            $('#display').text($(this_elem).text());
+
+        }
+        else{
+            $('#display').append($(this_elem).text());
+        }
 
 
-
-
-
+    }
 
 
 });
